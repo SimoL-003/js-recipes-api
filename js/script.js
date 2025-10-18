997198907c3cacc2f7f3b9de662686582eba0ac0
SimoL-003 <simoneleuzzi2003@gmail.com>
2025-10-18 13:58:28 +0200

    feat: add filter by tag

--- js/script.js
+++ js/script.js
@@ -1,54 +1,82 @@
 const cardsContainerElem = document.getElementById("cards-container");
 const tagsInputElem = document.getElementById("tags");
-const tagsInputValue = document.getElementById("tags").value;
 const formElem = document.querySelector("form");
 
-// axios.get("https://dummyjson.com/recipes/tags").then((resp) => {
-//   const tagsArray = [];
-//   for (let i = 0; i < resp.data.length; i++) {
-//     tagsArray.push(resp.data[i]);
-//   }
-//   tagsArray.sort();
-//   let tagsOption = `<option value="Qualsiasi">Tags</option>`;
-//   for (let i = 0; i < tagsArray.length; i++) {
-//     tagsOption += `<option value="${tagsArray[i]}">${tagsArray[i]}</option>`;
-//   }
-//   tagsInputElem.innerHTML = tagsOption;
+axios
+  .get("https://dummyjson.com/recipes?sortBy=name&order=asc")
+  .then((resp) => {
+    let card = "";
+    const tagsArray = [];
+    const difficultyArray = [];
+    const mealTypeArray = [];
+    const cuisineArray = [];
 
-//   formElem.addEventListener("submit", function (event) {
-//     event.preventDefault();
-//   });
-// });
+    for (let i = 0; i < resp.data.recipes.length; i++) {
+      const curRecipe = resp.data.recipes[i];
+      const { tags } = curRecipe;
 
-axios.get("https://dummyjson.com/recipes").then((resp) => {
-  let card = "";
-  const tagsArray = [];
-
-  for (let i = 0; i < resp.data.recipes.length; i++) {
-    let { name, image, tags, difficulty } = resp.data.recipes[i];
-    difficulty = difficulty.toLowerCase();
-
-    // FORM
-    // Creazione dei valori del select dei tag
-    let tagsOption = `<option value="Qualsiasi">Tags</option>`;
-    for (let ii = 0; ii < tagsArray.length; ii++) {
-      tagsOption += `<option value="${tagsArray[ii]}">${tagsArray[ii]}</option>`;
-    }
-    tagsInputElem.innerHTML = tagsOption;
-
-    // CARD
-    // Creazione badge per i tag da inserire nella card
-    tagsSpan = "";
-    for (let ii = 0; ii < tags.length; ii++) {
-      let curTag = tags[ii];
-      tagsSpan += `<span class="badge">${curTag}</span>`;
-      if (contains(tagsArray, curTag) === false) {
-        tagsArray.push(curTag);
+      // CARD
+      for (let ii = 0; ii < tags.length; ii++) {
+        const curTag = tags[ii];
+        // Creazione dell'array contenente tutti i tag
+        if (contains(tagsArray, curTag) === false) {
+          tagsArray.push(curTag);
+        }
       }
-    }
 
-    // Creazione card
-    card += `
+      // Creazione delle opzioni del select dei tag
+      let tagsOption = `<option value="">Tags</option>`;
+      tagsArray.sort();
+      for (let ii = 0; ii < tagsArray.length; ii++) {
+        tagsOption += `<option value="${tagsArray[ii]}">${tagsArray[ii]}</option>`;
+      }
+      tagsInputElem.innerHTML = tagsOption;
+
+      //   Creazione card tramite funzione
+      card += createCard(resp.data.recipes[i]);
+    }
+    cardsContainerElem.innerHTML = card;
+
+    // Tag filter
+    formElem.addEventListener("submit", function (event) {
+      event.preventDefault();
+      let recipeFiltered = [];
+      const tagsInputValue = document.getElementById("tags").value;
+      console.log(tagsInputValue);
+      for (let i = 0; i < resp.data.recipes.length; i++) {
+        const curRecipe = resp.data.recipes[i];
+        if (tagsInputValue && contains(curRecipe.tags, tagsInputValue)) {
+          recipeFiltered.push(curRecipe);
+        }
+      }
+      if (tagsInputValue === "") {
+        recipeFiltered = resp.data.recipes;
+      }
+      console.log(recipeFiltered);
+
+      card = "";
+      for (let i = 0; i < recipeFiltered.length; i++) {
+        card += createCard(recipeFiltered[i]);
+        console.log(card);
+      }
+      console.log(card);
+      cardsContainerElem.innerHTML = card;
+    });
+  });
+
+// -------------------- FUNZIONI --------------------
+// Funzione che crea la card (e i badge contenuti al suo interno)
+function createCard(recipe) {
+  let { name, image, tags, difficulty } = recipe;
+  difficulty = difficulty.toLowerCase();
+  let tagsSpan = "";
+  for (let ii = 0; ii < tags.length; ii++) {
+    const curTag = tags[ii];
+    // Creazione badge per i tag da inserire nella card
+    tagsSpan += `<span class="badge">${curTag}</span>`;
+  }
+  // Creazione card
+  card = `
                 <div class="card">
                     <div class="card__img">
                         <img src="${image}"
@@ -65,13 +93,13 @@ axios.get("https://dummyjson.com/recipes").then((resp) => {
                     </div>
                 </div>
     `;
-  }
-  cardsContainerElem.innerHTML = card;
-});
+  return card;
+}
 
-function contains(array, obj) {
+// Funzione per verificare se un elemento è contenuto in un array
+function contains(array, elem) {
   for (let i = 0; i < array.length; i++) {
-    if (array[i] === obj) {
+    if (array[i] === elem) {
       return true;
     }
   }
